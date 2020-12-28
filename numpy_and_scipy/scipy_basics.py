030488af72e2434d0854baa41aa23d1093dd7ec4
Cindy Grimm <grimmc@oregonstate.edu>
2020-12-28 14:08:25 -0800

    Fixes

---
+++ numpy_and_scipy/scipy_basics.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+from scipy.io import wavfile
+from scipy import fft
+import numpy as np
+
+
+def f(a=(0, 3)):
+    print(a)
+    a[0] = 12
+    #return a[0] * a[1]
+
+if __name__ == '__main__':
+
+    fname = 'foo.wav'
+    (sf, data) = wavfile.read('sinewave1000hz.wav')
+
+    magic_fft = fft.rfft(data)
+    print(magic_fft)
+
+    a = np.array([1, 3, 5, 6])
+    check = a < 5
+    a[check] = 0
+    print( a[check])
+    a = [0, 3]
+    f(a)
+    f(a)
+
+
