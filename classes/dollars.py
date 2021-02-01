99259778c4d1bfe3425bc3e7b9fc652f8fea5482
Cindy Grimm <grimmc@oregonstate.edu>
2021-02-01 11:06:10 -0800

    Adding more comments

--- classes/dollars.py
+++ classes/dollars.py
@@ -22,6 +22,9 @@ class Dollars:
         # represent physical money, with the smallest unit being one cent.  The constructor is where we enforce our
         # policy for converting to pennies.  This is one example of how to do it, but it's not the only one that's
         # possible.
+
+        # self.cents is NOT the same as the input parameter cents - it's a variable defined in
+        # Dollars that's named cents.
         self.cents = int(int(dollars * 100) + cents)
 
     def __repr__(self):
@@ -37,12 +40,20 @@ class Dollars:
         # To get the number of dollars, we take the absolute value of cents (which could be negative) and do integer
         # division.  To get the number of cents, we take the absolute number of cents modulo 100.  We need to use the
         # absolute values to make sure things come out right.
-        return '{0}${1:,}.{2:02}'.format('-' if self.cents < 0 else '', abs(self.cents) // 100, abs(self.cents) % 100)
+
+        # Doing it pedantically
+        str_neg_sign = '-' if self.cents < 0 else ''
+        str_dollar_amount = abs(self.cents) // 100  # Integer divide
+        str_cents_amount = abs(self.cents) % 100
+        str_glue_it_together = '{0}${1:,}.{2:02}'.format(str_neg_sign, str_dollar_amount, str_cents_amount)
+        return str_glue_it_together
+        # This is the same thing as above, just in one fell swoop
+        # return '{0}${1:,}.{2:02}'.format('-' if self.cents < 0 else '', abs(self.cents) // 100, abs(self.cents) % 100)
 
     def __str__(self):
         """
         The string representation of Dollars.
-        :return:
+        :return: a string
         """
 
         # This is just going to be the same as the representation.
@@ -74,8 +85,13 @@ class Dollars:
         # raise an exception, and we'll leave the user to deal with it.
         try:
             # Assume other is an instance of Dollars.
+            #   The cents=self.cents sets the cents value directly - could have done
+            #   Dollars(0, self.cents + other.cents)
             return Dollars(cents=self.cents + other.cents)
         except AttributeError:
+            # AttributeError happens when you try to do foo.bar and bar is not a variable
+            #  or methods on the class foo
+
             # If other is not an instance of Dollars, try to make one from it, and recursively call __add__.
             return self + Dollars(other)
 
@@ -109,7 +125,7 @@ class Dollars:
         :return: The result of the subtraction.
         """
 
-        # Duck typing one more time.
+        # Duck typing one more time - see add above.
         try:
             return Dollars(cents=other.cents - self.cents)
         except AttributeError:
@@ -151,18 +167,47 @@ if __name__ == '__main__':
     # What sort of testing would you do for the Dollars class?
 
     # Make an instance of the Dollars class
-    d = Dollars(1, 23)
+    d = Dollars(1, 23)  # dollars is set to 1, cents is set to 23
     print(d)
-    print(3 * d)
+
+    # If you don't give two arguments, defaults to dollars = 10
+    d_ten_dollars = Dollars(10)
+    print(d_ten_dollars)
+
+    # .. But you can explicitly set cents
+    d_ten_cents = Dollars(cents=10)
+    print(d_ten_cents)
+
+    # Call the negative (__neg__) method on Dollars and store the result in d_neg
+    d_neg = - d
+    print(d_neg)
+
+    # Calls __rmul__ on the Dollar class, then creates a Dollar from that and prints it
+    d_times_three_v1 = 3 * d
+    print(d_times_three_v1)
+
+    # Calls __mul__ on the Dollar class, then creates a Dollar from that and prints it
+    d_times_three_v2 = d * 3
+    print(d_times_three_v2)
 
     # Since Dollars behaves like a numerical quantity (and, in particular, has __add__defined for this example), we
     # can use functions on it, just like it was a built-in numerical type.
     l = [Dollars(x) for x in range(5)]
     print(l)
+
+    # Show just adding two Dollar amounts together
+    l2_l4 = l[2] + l[4]
+    # Show adding a number to a dollar
+    l3 = l[0] + 2  # 2 will turn into $2. Note that PyCharm is flagging that it thinks the type int is wrong
+    l4 = 3 + l[0]  # 3 will turn into $3
+
+    # Because + is defined, we can sum the list (which just does a bunch of adds)
     print(sum(l))
 
     # If you want to, you can also use the raw forms of the functions, but it looks ugly.
-    a = Dollars(1.45)
+    a = Dollars(1, 45)
     b = Dollars(cents=25)
-    c = a.__add__(b)
+    c = a.__add__(b)   # Exactly the same as c = a + b
+    c_better = a + b
     print(a, b, c)
+    print(a, b, c_better)
