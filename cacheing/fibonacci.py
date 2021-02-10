65a35e8579c9eed9aebada9fa9ed99fbbb6178c5
Cindy Grimm <grimmc@oregonstate.edu>
2021-02-10 08:44:29 -0800

    Added comments. Added fourth version (cached and instrumented).

--- cacheing/fibonacci.py
+++ cacheing/fibonacci.py
@@ -1,5 +1,19 @@
 #!/usr/bin/env python3
 
+""" This script illustrates the following concepts:
+      Caching - how to save calculations in a variable to speed up computation
+      Timing comparisons - how to use time() to figure out how long
+          a computation took
+      Try-except to catch parameter errors and to determine if
+          the caching variables exist
+      Associating attributes (variables) with functions
+         - using default function attributes, like __name__
+    Four versions: Caching y/n and instrumented for timing y/n
+
+    Note: If you were to do this "cleanly" you would put all this in a
+          class.
+      """
+
 
 def fib_basic(n):
     """
@@ -20,6 +34,41 @@ def fib_basic(n):
         return fib_basic(n - 1) + fib_basic(n - 2)
 
 
+def fib_cached(n):
+    """
+    Adding caching to fib_basic
+    :param n: The Fibonacci number to calculate.
+    :return: The Fibonacci number
+    """
+
+    # Make sure the inputs are valid
+    if n < 0:
+        raise ValueError('Argument must be non-negative')
+
+    # Try to return the value from the cache.
+    try:
+        #   Yes, you can do this - remember, functions are just dictionaries (stored
+        #    in a dictionary of functions).
+        #   So you can add an entry (variable) to that dictionary
+        #   Note - we have to actually *declare* the cacge variable below, or it won't
+        #    exist when we try to access it.
+        return fib_cached.cache[n]
+    except KeyError:
+        # Calculate the new value
+        retval = fib_cached(n - 1) + fib_cached(n - 2)
+
+        # Add it to the cache
+        fib_cached.cache[n] = retval
+
+        # Return the value.
+        return retval
+
+
+# This attribute, associated with the function above,
+# is a casche. Initialize the cache with the base case values.
+fib_cached.cache = {0: 0, 1: 1}
+
+
 def fib_instrumented(n):
     """
     Simple Fibonacci function, defined recursively, instrumented to count calls.
@@ -28,6 +77,11 @@ def fib_instrumented(n):
     """
 
     # Increment the call count
+    #   Yes, you can do this - remember, functions are just dictionaries (stored
+    #    in a dictionary of functions).
+    #   So you can add an entry (variable) to that dictionary
+    #   Note - we have to actually *declare* this variable below, or it won't
+    #    exist when we try to add 1 to it.
     fib_instrumented.call_count += 1
 
     # Check to make sure the argument is valid.
@@ -42,18 +96,19 @@ def fib_instrumented(n):
         return fib_instrumented(n - 1) + fib_instrumented(n - 2)
 
 
-# This attribute, associated with the function, will let us keep track of the number of calls.
+# This attribute, associated with the function above, will let us keep track of the number of calls.
 fib_instrumented.call_count = 0
 
 
-def fib_cached(n):
+def fib_cached_instrumented(n):
     """
-    This Fibonacci function uses cacheing to speed things up.  It's also instrumented, to show how
+    This Fibonacci function uses caching to speed things up.
+    It's also instrumented, to show how many function calls are taken
     :param n:
     :return:
     """
-    # Increment the call count
-    fib_cached.call_count += 1
+    # Increment the call count - see fib_instrumented for explanation
+    fib_cached_instrumented.call_count += 1
 
     # Check to make sure the argument is valid.
     if n < 0:
@@ -62,21 +117,21 @@ def fib_cached(n):
     # Try to return a number from the cache.  If that fails, then we have to calculate it (recursively) from scratch,
     # add it to the cache, and then return it.
     try:
-        return fib_cached.cache[n]
+        return fib_cached_instrumented.cache[n]  # Again, this is a variable defined on the function
     except KeyError:
         # Calculate the value recursively
-        retval = fib_cached(n - 1) + fib_cached(n - 2)
+        retval = fib_cached_instrumented(n - 1) + fib_cached_instrumented(n - 2)
 
         # Store it in the cache
-        fib_cached.cache[n] = retval
+        fib_cached_instrumented.cache[n] = retval
 
         # And, finally, return it
         return retval
 
 
-# Initialize the cache with the two base cases.
-fib_cached.call_count = 0
-fib_cached.cache = {0: 0, 1: 1}
+# Initialize the cache with the two base cases and the counter with 0.
+fib_cached_instrumented.call_count = 0
+fib_cached_instrumented.cache = {0: 0, 1: 1}
 
 
 def time_fib(f, n):
@@ -86,7 +141,12 @@ def time_fib(f, n):
     :param n: The Fibonacci number to evaluate.
     :return: The Fibonacci number.
     """
+    # You can put the import here - it will only happen if time_fib is called
+    from time import time
+
     # Reset the call count
+    #   For the functions that *don't* have instrumentation, this will
+    #    create that variable
     f.call_count = 0
 
     # Time a call to the Fibonacci function
@@ -98,48 +158,26 @@ def time_fib(f, n):
     return stop_time - start_time, f.call_count
 
 
-def fib(n):
-    """
-    A clean version of the cached Fibonacci number function.
-    :param n: The Fibonacci number to calculate.
-    :return: The Fibonacci number
-    """
-
-    # Make sure the inputs are valid
-    if n < 0:
-        raise ValueError('Argument must be non-negative')
-
-    # Try to return the value from the cache.
-    try:
-        return fib.cache[n]
-    except KeyError:
-        # Calculate the new value
-        retval = fib(n - 1) + fib(n - 2)
-
-        # Add it to the cache
-        fib.cache[n] = retval
-
-        # Return the value.
-        return retval
-
-
-# Initialize the cache, as an attribute of the function, with the base case values.
-fib.cache = {0: 0, 1: 1}
-
-
 if __name__ == '__main__':
-    from time import time
+    # You can put the imports here
     import matplotlib.pyplot as plt
 
     # Do some tests to make sure fibonacci functions are working as expected
+    #   This is a hard-coded set of correct answers to test against
     fib_tests = {0: 0, 1: 1, 2: 1, 3: 2, 4: 3, 5: 5, 6: 8, 7: 13}
-    functions = [fib_basic, fib_instrumented, fib_cached]
+    #   This is an array of the functions we're going to test
+    #     Notice we're not calling the functions (no ()) - we're just putting the
+    #     function variables in the list
+    functions = [fib_basic, fib_cached, fib_instrumented, fib_cached_instrumented]
 
     # Test each of the functions on all of the test cases.
-    for func in functions:
-        print('{0}():'.format(func.__name__))
+    for func in functions:  # Yes, this works - func is one of the fib_* functions
+        print('{0}():'.format(func.__name__))  # .__name__ is defined on every function
 
+        # Since fib_tests is a dictionary, and we want both the key and
+        #  the value, we use the .items() iteration method
         for number, value in fib_tests.items():
+            # Call the function and compare it to the test value
             if func(number) != value:
                 print('  failed for {0}: expected {1}, got {2}'.format(number, value, func(number)))
 
@@ -148,6 +186,8 @@ if __name__ == '__main__':
             func(-1)
             print('  failed for negative value')
         except ValueError:
+            # This is the error the functions should throw if given a -1 - so it
+            #   passed the test, don't do anything
             pass
 
         print('  tests complete')
@@ -155,9 +195,9 @@ if __name__ == '__main__':
     # Do some timing tests to see how long things take.  We're going to look at wall clock time and also
     # at the number of calls we make to the fib() function.  To do that, we're going to make a new version
     # of the function that's instrumented to count the number of calls, which we will call fib2().
-    N = 15
+    N = 15   # test on 1 to 15
     times, counts = zip(*(time_fib(fib_instrumented, n) for n in range(N)))
-    cached_times, cached_counts = zip(*(time_fib(fib_cached, n) for n in range(N)))
+    cached_times, cached_counts = zip(*(time_fib(fib_cached_instrumented, n) for n in range(N)))
 
     # Let's plot the times and the call counts
     fig, (ax1, ax2) = plt.subplots(2, 1, sharex=True)
